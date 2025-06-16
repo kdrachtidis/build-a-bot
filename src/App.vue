a150fadb916fd55fc6293eebb7f950728fd610be
Konstantinos Drachtidis <kdrachtidis@outlook.com>
2025-06-16 16:29:53 +0200

    Kapitel 4

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo" />Build a bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <!--<HomePage msg="You did it!" />-->
+    <RobotBuilder />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue'
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HomePage,
+    RobotBuilder,
   },
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  width: 1400px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1460px;
+  margin: 0 auto;
+}
+ul {
+  padding: 30px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  font-size: 22px;
+  padding: 5px 10px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" alt="Robot" />
+    </div>
+    <div class="get-started"><a href="">Get started</a> building your first robot!</div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 500px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 32px;
+}
+</style>
