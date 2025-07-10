fb6329af8f9dd0bcde4fe8809458ce4b10eadf50
Konstantinos Drachtidis <kdrachtidis@outlook.com>
2025-07-10 21:39:51 +0200

    Module 11

--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,20 @@ module.exports = defineConfig({
       },
     },
   },
+  configureWebpack: (config) => {
+    const newRule = {
+      test: /\.(png|jpe?g|gif|webp|avif)(\?.*)?$/,
+      type: 'asset',
+      generator: {
+        filename: 'img/[name].[hash:16][ext]',
+      },
+    };
+
+    const imagesRuleIndex = config.module.rules.findIndex((x) => x.test.source.includes('png|jpe?g'));
+    config.module.rules.splice(imagesRuleIndex, 1, newRule);
+    config.module.rules.push({
+      test: /\.coffee$/,
+      use: ['coffee-loader'],
+    });
+  },
 });
